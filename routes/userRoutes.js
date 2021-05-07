34e1a9e74772abef8b503f1b41d72ae0ce2535d7
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-05-07 16:21:05 -0400

    Add /register, /login routes and createUser, getUserByEmail queries

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,79 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const { createNewUser } = require("../db/queries/users");
+const { createUser, getUserByEmail } = require("../db/queries/users");
 
 // Register a new user
 router.post("/register", (req, res) => {
-  // TODO: check user is not logged in (ie. no cookie)
+  // TODO: check user is not logged in
+  // const decoded = jwt.verify(req.body.token, process.env.JWT_SECRET_KEY)
 
   // Check form is valid
-  const { firstName, lastName, email, password } = req.body;
+  let { firstName, lastName, email, password } = req.body;
   if (!firstName || !lastName || !email || !password) {
     res.status(400).send();
   }
 
-  createNewUser({ firstName, lastName, email, password });
+  // Hash password
+  password = bcrypt.hashSync(password, 10);
+
+  // Save the user
+  createUser({ firstName, lastName, email, password })
+    .then((result) => {
+      if (result.length > 0) {
+        // Save was successful
+        const token = jwt.sign({ id: result.id }, process.env.JWT_SECRET_KEY);
+        res.status(200).send({ token, email });
+      } else {
+        res.status(400).send();
+      }
+    })
+    .catch((err) => res.status(500).send(err));
+});
+
+// Log a user in
+// If successful, returns a token and user's email
+router.post("/login", (req, res) => {
+  // TODO: check user is not currently logged in
+
+  // Check we have email and password
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).send();
+  }
+
+  // Find user
+  getUserByEmail(email)
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(400)
+          .send({ message: "No user exists with this email" });
+      }
+
+      // User exists - check password
+      bcrypt
+        .compare(password, user.password)
+        .then((match) => {
+          if (match) {
+            // Valid login - set JWT and send
+            const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY);
+            res.status(200).send({ token, email });
+          } else {
+            // Invalid password
+            res.status(401).send({ message: "Invalid password" });
+          }
+        })
+        .catch((err) => {
+          // error occurred with bcrypt compare
+          return res.status(500).send(err);
+        });
+    })
+    .catch((err) => {
+      // error occurred with query
+      return res.status(500).send(err);
+    });
 });
 
 module.exports = router;
