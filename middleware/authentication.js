59c237258c8f5ac72c0722f4bf95cc9063d932f5
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-05-10 12:25:28 -0400

    clean up authentication middlware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,4 +1,3 @@
-const { response } = require("express");
 const jwt = require("jsonwebtoken");
 
 const isAuthenticated = function (req, res, next) {
@@ -10,14 +9,7 @@ const isAuthenticated = function (req, res, next) {
     );
     next();
   } catch (e) {
-    if (e.message === "jwt must be provided") {
-      return res.status(401).send({ message: "User has no JWT" });
-    } else if (e.message === "invalid token") {
-      return res.status(401).send({ message: "User has invalid JWT" });
-    } else {
-      console.log(e);
-      return res.status(401).send({ message: `${e.name}: ${e.message}` });
-    }
+    res.status(401).send({ message: `${e.name}: ${e.message}` });
   }
 };
 
