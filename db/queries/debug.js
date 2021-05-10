5783eeded671ccd7638f8642564cf8650af13c75
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-05-10 12:58:17 -0400

    formatting, add DB reset confirmation/error

--- db/queries/debug.js
+++ db/queries/debug.js
@@ -4,14 +4,15 @@ const path = require("path");
 
 const resetDb = function () {
   const schemaPath = path.join(__dirname, "..", "schema", "create.sql");
-  fs.readFile(schemaPath, { encoding: "utf-8" }).then((contents) =>
-    db.query(contents).then(() => {
+  fs.readFile(schemaPath, { encoding: "utf-8" })
+    .then((contents) => db.query(contents))
+    .then(() => {
       const seedPath = path.join(__dirname, "..", "schema", "seed.sql");
-      fs.readFile(seedPath, { encoding: "utf-8" }).then((contents) =>
-        db.query(contents)
-      );
+      return fs.readFile(seedPath, { encoding: "utf-8" });
     })
-  );
+    .then((contents) => db.query(contents))
+    .then(() => console.log("DB reset complete"))
+    .catch((e) => console.log("Error during DB reset: ", e));
 };
 
 module.exports = { resetDb };
--- routes/debugRoutes.js
+++ routes/debugRoutes.js
@@ -4,7 +4,7 @@ const { resetDb } = require("../db/queries/debug");
 
 router.get("/reset_db", (req, res) => {
   resetDb();
-  console.log('db reset')
+  console.log("DB reset initiated");
   res.end();
 });
 
