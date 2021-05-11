804d00da2afa981dbdd25bd77dd362c5cd1c8e23
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-05-11 16:42:58 -0400

    update bookmarks routes to use error handler

--- routes/bookmarkRoutes.js
+++ routes/bookmarkRoutes.js
@@ -1,26 +1,26 @@
 const { addBookmark, deleteBookmark } = require("../db/queries/bookmarks");
 const router = require("express").Router();
 
-router.post("/bookmarks", (req, res) => {
+router.post("/bookmarks", (req, res, next) => {
   const { id } = res.locals.decodedToken;
   const { postID } = req.body;
   if (!postID) {
-    return res.status(400).send({ message: "postID is required" });
+    return next({ status: 400, message: "postID is required" });
   }
   addBookmark(id, postID)
     .then((result) => res.send())
-    .catch((e) => res.status(500).send(e));
+    .catch((err) => next(err));
 });
 
-router.delete("/bookmarks", (req, res) => {
+router.delete("/bookmarks", (req, res, next) => {
   const { id } = res.locals.decodedToken;
   const { postID } = req.body;
   if (!postID) {
-    return res.status(400).send({ message: "postID is required" });
+    return next({ status: 400, message: "postID is required" });
   }
   deleteBookmark(id, postID)
     .then((result) => res.send())
-    .catch((e) => res.status(500).send(e));
+    .catch((err) => next(err));
 });
 
 module.exports = router;
