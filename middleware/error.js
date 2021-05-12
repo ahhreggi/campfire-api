586104be6acb4cbecab0bbf13d9d332a943d267c
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-05-11 23:55:03 -0400

    update error handler message

--- middleware/error.js
+++ middleware/error.js
@@ -1,8 +1,9 @@
 const handleErrors = (err, req, res, next) => {
+  console.log(err);
   const { status, message } = err;
   if (status && message) {
     res.status(status).send({ message });
-  } else res.status(500).send(err);
+  } else res.status(500).send({ message: `Unknown error: ${err.message}` });
 };
 
 module.exports = { handleErrors };
