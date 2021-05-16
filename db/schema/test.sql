b3394f08b4054397c05c202afd3e11874f972011
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-05-16 11:11:51 -0400

    testing

--- db/schema/test.sql
+++ db/schema/test.sql
@@ -1,11 +1,6 @@
-SELECT 
-  users.id,
-  users.first_name,
-  users.last_name,
-  users.email,
-  users.avatar_id,
-  enrolments.role
-FROM courses
-INNER JOIN enrolments ON courses.id = course_id
-INNER JOIN users ON user_id = users.id
-WHERE courses.id = 1;
+SELECT post_views.user_id, post_views.post_id
+FROM post_views
+INNER JOIN posts ON posts.id = post_id
+INNER JOIN courses ON courses.id = course_id
+WHERE courses.id = 1
+AND post_views.user_id = 1;
