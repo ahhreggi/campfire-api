548beb5153512b5c0f4dd0a5be1435f539b47394
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-05-19 22:24:28 -0400

    add ssl

--- db/index.js
+++ db/index.js
@@ -3,6 +3,7 @@ const { Pool } = require("pg");
 let dbParams = {};
 if (process.env.DATABASE_URL) {
   dbParams.connectionString = process.env.DATABASE_URL;
+  dbParams.ssl = true;
 } else {
   dbParams = {
     host: process.env.DB_HOST,
